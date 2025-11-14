97c424502657cbac74a4163e15c2770c08a06b57
Alexis Schmidt <alexisfsch.3@gmail.com>
2025-11-14 18:00:43 -0300

    Carga de atributos y relaciones en Estadia

--- G30/src/main/java/tp/g30/clases/Estadia.java
+++ G30/src/main/java/tp/g30/clases/Estadia.java
@@ -4,10 +4,47 @@
  */
 package tp.g30.clases;
 
-/**
- *
- * @author juanc
- */
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
 public class Estadia {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Relación: Una estadía está asociada a UNA habitación.
+    @ManyToOne
+    @JoinColumn(name = "habitacion_id", nullable = false)
+    private Habitacion habitacion;
+
+    private Float valor_estadia;
+
+    // Relación: Una estadía puede tener varios huéspedes.
+    @ManyToMany
+    @JoinTable(
+        name = "estadia_huesped",
+        joinColumns = @JoinColumn(name = "estadia_id"),
+        inverseJoinColumns = @JoinColumn(name = "huesped_id")
+    )
+    private List<Huesped> lista_huespedes = new ArrayList<>();
+
+    // Relación: Una estadía puede tener varias facturas.
+    @OneToMany
+    @JoinColumn(name = "estadia_id")
+    private List<Factura> lista_facturas = new ArrayList<>();
+
+    // Relación: Una estadía puede tener muchos consumos.
+    @OneToMany
+    @JoinColumn(name = "estadia_id")
+    private List<Consumo> lista_consumos = new ArrayList<>();
+
+    private LocalDate fecha_check_in;
+    private LocalDate fecha_check_out;
 }
+
