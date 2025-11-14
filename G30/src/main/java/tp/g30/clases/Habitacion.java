97fe624c7e4f9efb84209e6c3355bc2e3fcc003e
martinbuxman <cesarmartinbuxmancramaro@gmail.com>
2025-11-14 16:15:59 -0300

    Se agrego atributos y configuracion de entidad Habitacion

--- G30/src/main/java/tp/g30/clases/Habitacion.java
+++ G30/src/main/java/tp/g30/clases/Habitacion.java
@@ -1,10 +1,21 @@
 package tp.g30.clases;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Habitacion {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long numeroHabitacion;
+    private String tipo;
+    private int cantidadHuespedes;
+    private int cantidadCamaI;
+    private int cantidadCamaD;
+    private int cantidadCamaKS;
 }
